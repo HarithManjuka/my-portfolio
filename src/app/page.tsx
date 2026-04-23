aab9fa04fece4b18ffccc259989c3929698c6eb7
HarithManjuka <harithmanjukaabekon11@gmail.com>
2026-04-23 19:46:59 +0530

    chore: setup clean project structure

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white">
+      <h1 className="text-4xl font-bold text-center mt-20">
+        Harith Manjuka Portfolio
+      </h1>
+    </main>
+  );
+}
